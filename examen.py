3381f35aea87d7f475344569f634d790b2b5ec74
PABLO <pablom.espinosal@uqvirtual.edu.co>
2022-10-02 00:54:42 -0500

    tercer commit

--- examen.py
+++ examen.py
@@ -1,4 +1,8 @@
 from math import sqrt
+import matplotlib.pyplot as plt
+import numpy as np 
+from mpl_toolkits.mplot3d import Axes3D
+
 m=10
 class Vector:
     def __init__(self, x=0, y=0, z=0):
@@ -26,6 +30,14 @@ class Vector:
         return(cadena)
     def Aceleracion(self, m):
         return(Vector(self.x/m, self.y/m, self.z/m))
+# Creamos la figura
+fig = plt.figure()
+
+# Agrrgamos un plano 3D
+ax1 = fig.add_subplot(111,projection='3d')
+
+# Mostramos el gráfico
+plt.show()  
     
 #Bloque Principal
 #Vectores 
@@ -46,6 +58,12 @@ Vector8=Vector1.ProductoPunto(Vector2,Vector3)
 #Vector9=Vector8.ProductoPunto(Vector3)
 PCV1V2=Vector1.ProductoCruz(Vector2)
 Vectoraceleracion = Vector5.Aceleracion(m)
+#calculos de las cordenadas de posicion en funcion del tiempo 
+#pasados un 60 segundos 
+cordenadax = float(((Vectoraceleracion.x * (60)**2)/2))
+cordenaday = float(((Vectoraceleracion.y * (60)**2)/2))
+cordenadaz = float(((Vectoraceleracion.z * (60)**2)/2))
+print(cordenadax,cordenaday,cordenadaz)
 #Imprimir
 print("V1+V2+V3:", Vector5)
 print("V1-V2-V3:", Vector7)
@@ -55,3 +73,18 @@ print("Norma V3:", round(Norma3,2))
 print("V1*V2*V3:",Vector8 )
 print("V1xV2:", PCV1V2)
 print("Aceleracion",Vectoraceleracion  )
+print(type(Vectoraceleracion))
+r = np.array([cordenadax,cordenaday,cordenadaz])
+s = np.array([4,5,6])
+
+def trazado_vectores_3d(ax,vectores,**kwords):
+    x_cor,y_cor,z_cor = zip(*vectores)
+    ax.scatter(x_cor,y_cor,z_cor,**kwords)
+    for v in vectores:
+        x,y,z = v
+        ax.plot([x,x],[y,y],[0,z], color = "gray",linestyle="dotted",marker = ".")
+        
+subplot3d = plt.subplot(111,projection="3d")
+trazado_vectores_3d(subplot3d,[r,s], color = ["r","b"])
+subplot3d.set_zlim([0,8])
+plt.show()
